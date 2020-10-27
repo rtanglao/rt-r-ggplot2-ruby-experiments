0985921b03bec8ab19493a8dc75290b1277602c4
Roland Tanglao <roland@rolandtanglao.com`>
2020-10-26 23:24:53 -0700

    first working version of the hourly script

--- hourly-ff-question-barcode.R
+++ hourly-ff-question-barcode.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
 library(tidyverse)
+library(tibbletime)
+library(lubridate)
+
 colours_array <- colours()
 
 getOSColour <- function(.x) { 
@@ -40,21 +43,38 @@ if ("--help" %in% args) {
   q(save = "no")
 }
 print(script.name)
-stop()
+
 main <- function() {
-  csv <-read.csv(
-      "https://raw.githubusercontent.com/rtanglao/rt-kits-api3/main/2020/2020-10-20-2020-10-20-firefox-creator-answers-desktop-all-locales.csv"
-  )
+  filename <-
+    paste0(
+      args[1], "-",
+      args[2], "-",
+      args[3], "-",
+      args[1], "-",
+      args[2], "-",
+      args[3],
+      "-firefox-creator-answers-desktop-all-locales.csv"
+    )
+  csv <- read_csv(filename)
   # see https://stackoverflow.com/questions/39975317/how-to-reverse-the-order-of-a-dataframe-in-r
   reverse_csv <- csv %>% map_df(rev)
-  length <- nrow(reverse_csv)
+  csv_time <- reverse_csv %>%
+    mutate(created_time = parse_date_time(created, orders = "ymdHMSz"))
+  csv_tt <- as_tbl_time(csv_time, index = created_time)
+  time_filter_str <- paste0(args[1], "-", args[2], "-", args[3], " ", args[4])
+  hourly_csv <- csv_tt %>%
+    filter_time(time_filter_str ~ time_filter_str)
+
+  length <- nrow(hourly_csv)
+  options(tibble.width = Inf)
+  print(hourly_csv)
   xintercept <- seq.int(by = 10,
                         length = length * 2,
                         from = 0)
   size <- rep(c(4, 2), length = length * 2)
-  os_colours_vector <- map_chr(reverse_csv$tags, getOSColour)
-  question_colours_vector <- map2_chr(reverse_csv$title,
-                                      reverse_csv$content,
+  os_colours_vector <- map_chr(hourly_csv$tags, getOSColour)
+  question_colours_vector <- map2_chr(hourly_csv$title,
+                                      hourly_csv$content,
                                       ~ {
                                         colours_array[(abs(digest::digest2int(paste(.x, .y))) %% 657) + 1]
                                       })
@@ -64,6 +84,12 @@ main <- function() {
   p <- p + geom_vline(col = colours_vector,
                       xintercept = xintercept,
                       size = size)
+  png_filename <-   paste0("hour-", args[4], "-", args[1], "-", args[2], "-", 
+                           args[3], "-ff-desktop-hourly.png")
+  ggsave(png_filename, p, width = 26.666666667, height = 26.666666667, 
+         dpi = 72, limitsize = FALSE) # 26.6666667 = 1920/72dpi
+  warnings()
+
 }
 
 sink("log.txt")
